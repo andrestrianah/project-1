918cab03306a45d261fb679585777b5b8e4b6c9b
Nestor <andrestrianah97@gmail.com>
2021-12-13 10:35:08 -0800

    css 80%

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 BLACKJACK 
 
-A fast paced game, the objective is to get a card count of 21 or as close (without exceding) as you can.In order to win you have to beat the dealer, whoever has the best hand wins .
+A fast paced game, the objective is to get a card count of 21 or as close (without exceding) as you can.In order to win you have to beat the dealer, whoever has the best hand wins . Every time the dealer has a count of 17 or more, the dealer has to stand(hold the total and end the turn), otherwise , he must hit(request an extra card).
 
 GOOD LUCK.
+
+User Stories
+
+Me, as a player ,...
+* I want to see my count, so I can focus more on the play that in the math of the cards.
+* I want to have a double button, so I can win more if I got a good hand.
+
---
+++ js/main.js
@@ -0,0 +1,29 @@
+/*1. Defining the constant variables .In the particular case of blackjack,the constants will be the deck (value of the cards and the suits)
+,the limit(21),the naturals or blackjack (A's + ten) and the value of the chips that are going to be used for the bet.
+
+Also, add the images of each card and some sounds for the game.
+
+2.It's time for the state variables, do not set a value right now. 
+
+Set a variable for the player's hand, the computer's hand , the money that the player has (the bank), the bet, the deck(needs to be shuffled when there is no more cards, the cards that have been dealt and have to be placeit again on the deck, a stay variable and the results of the game (winner) 
+
+bonus: split variable (if the first two cards of the player are the same, split the cards and play with two hands with the initial amount of the bet for each hand)
+
+3.Cache elements that need to be updated in the htm:
+
+buttons(hit,replay,double,play again,split,etc) , player and computer cards , the chips, the player's hand count,the wining message and the bank.
+
+4.add the event listeners for the buttons (hit,replay,double,play again,split,etc)
+
+5.call the init function. The purpose of init, is to initialize our state variables for the start of the game, so give the initial values for the init function.
+
+6. invoke the render function to transfer all the data that needs to be update on the dom. 
+
+for the blackjack , you first will need to update the bank and the bet, after that, give the cards for the player and the computer and count the player cards.Then , if the player cards are over 21 or if it is lower to the computers hand(not over 21), computer wins and pop de message for the winner, if the player has a better hand than the computer or if the computer is bust(over 21), pop the message for the player. if both players have the same count, the player get the money back and is a tie.
+if the first two cards of the player are an A's and a ten, player wins unless computer has a blackjack too. 
+
+also give a function for the split and double option.
+
+7. reset the game by updating the new values for the bank and place the cards that were used in the bottom of the deck.
+
+*/
