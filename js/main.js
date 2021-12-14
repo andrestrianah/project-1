9ece60f5a38fb37dead3d5ed133d47dfb7bfdb26
Nestor <andrestrianah97@gmail.com>
2021-12-14 08:58:36 -0800

    state variables

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /*1. Defining the constant variables .In the particular case of blackjack,the constants will be the deck (value of the cards and the suits)
 ,the limit(21),the naturals or blackjack (A's + ten) and the value of the chips that are going to be used for the bet.*/
+console.log("is working");
 
 const suits = ["s", "c", "d", "h"];
 const ranks = [
@@ -22,14 +23,25 @@ const limit = 21;
 
 const masterDeck = buildMasterDeck();
 
+
+
+
+
 // Also, add the images of each card and some sounds for the game.
 
 // 2.It's time for the state variables, do not set a value right now. 
 
-// Set a variable for the player's hand, the computer's hand , the money that the player has (the bank), the bet, the deck(needs to be shuffled when there is no more cards, the cards that have been dealt and have to be placeit again on the deck, a stay variable and the results of the game (winner) 
+// Set a variable for the player's hand, the computer's hand , the money that the player has (the bank), the bet, the deck(needs to be shuffled when there is no more cards, the cards that have been dealt and have to be placeit again on the deck, a stand variable and the results of the game (winner) 
 
 // bonus: split variable (if the first two cards of the player are the same, split the cards and play with two hands with the initial amount of the bet for each hand)
-
+let newDeck;
+let playerTotal;
+let playerHand = [];
+let cpuTotal;
+let cpuHand = [];
+let winner = null;
+let shuffledDeck;
+let stand;
 // 3.Cache elements that need to be updated in the htm:
 
 // buttons(hit,replay,double,play again,split,etc) , player and computer cards , the chips, the player's hand count,the wining message and the bank.
